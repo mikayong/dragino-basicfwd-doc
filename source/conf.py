3ccc2252b706970a7c6d95d7a7eb166b12c34099
skerlan <yaogones@gmail.com>
2025-03-15 21:36:53 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -20,9 +20,9 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
+# html_theme = 'alabaster'
 html_static_path = ['_static']
